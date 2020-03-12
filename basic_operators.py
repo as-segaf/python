275fe3fa8412f9b27336f1adabee288581d8f73f
as-segaf <ziyanassegaf20@gmail.com>
2020-03-12 13:52:07 +0700

    operators

---
+++ basic_operators.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+a = 5
+b = 2
+
+## arithmetic operators
+print(a+b)  # 7
+print(a-b)  # 2
+print(a*b)  # 10
+print(a/b)  # 2.5
+print(a%b)  # 1
+print(a**b) # 25
+print('\n')
+
+## comparison operators
+print(a==b) # false
+print(a!=b) # true
+print(a>b)  # true
+print(a<b)  # false
+print(a>=b) # true
+print(a<=b) # false
+print('\n')
+
+## assigment operators
+# c = a+b
+# c += a is equivalent to c = c + a
+# c -= a is equivalent to c = c - a
+# c *= a is equivalent to c = c * a
+# c /= a is equivalent to c = c / a
+# c %= a is equivalent to c = c % a
+# c **= a is equivalent to c = c ** a
+
+## logical operators
+x = True
+y = False
+
+print(x and y)  # If both the operands are true then condition becomes true.
+print(x or y)   # If any of the two operands are non-zero then condition becomes true.
+print(not x)    # Used to reverse the logical state of its operand.
+print('\n')
