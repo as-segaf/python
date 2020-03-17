6eba8f1eb7cbc725ad69934a1baaa08668951003
as-segaf <ziyanassegaf20@gmail.com>
2020-03-17 13:59:54 +0700

    global & local variable

---
+++ variable-scope.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+## Global and local scope
+
+total = 0   # this is glbal variable
+def sum(arg1,arg2):
+    total = arg1 + arg2 # here is local variable
+    print('value inside the function:',total)
+    return total
+
+sum(10,40)
+print("value outside the function:",total)
