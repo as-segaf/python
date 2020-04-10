2b540f0a0093888d6ea0d67be432b77e43e5c997
as-segaf <ziyanassegaf20@gmail.com>
2020-04-10 08:27:28 +0700

    sets

--- data-types.py
+++ data-types.py
@@ -2,10 +2,10 @@
 
 ## python string
 
-stri = "hello world !"
+str = "hello world !"
 
 # str[0]="t"                                                  # error, string cannot be changed
-print(f"string {stri} asfkasf")
+print(f"string {str} asfkasf")
 print(str)                                                  # hello world !
 print(str[0])                                               # h
 print(str[2:5])                                             # llo 
@@ -55,7 +55,7 @@ print('\n')
 list[1] = 999       # valid syntax with list
 print(list[1])
 print('\n')
-tuple[1] = 999      # invalid syntax with tuple
+# tuple[1] = 999      # invalid syntax with tuple
 
 ## python dictionary (similar to object)
 # use curl braces {}
@@ -66,8 +66,41 @@ dict[2]     = "This is two"
 
 tinydict = {'name': 'john','code':6734, 'job': 'pilot'}
 
-print (dict['one'])       # "This is one"
-print (dict[2])           # "This is two"
-print (tinydict)          # {'name': 'john','code':6734, 'job': 'pilot'}
-print (tinydict.keys())   # dict_keys(['code', 'ame', 'job'])
-print (tinydict.values()) # dict_values([6734, 'john', 'pilot'])
+print (dict['one'])         # "This is one"
+print (dict[2])             # "This is two"
+print (tinydict)            # {'name': 'john','code':6734, 'job': 'pilot'}
+print (tinydict.keys())     # dict_keys(['code', 'ame', 'job'])
+print (tinydict.values())   # dict_values([6734, 'john', 'pilot'])
+
+
+## python set
+# use curl braces {}
+
+myset = {1,'qwe',3.5,'qwe'}     # cannot contain duplicate elements
+anotherset = {1,2,4,3}          # set will reorder automatically
+print(type(myset))
+print(myset)
+print(anotherset)
+
+# set union
+# combine given sets
+
+set1,set2,set3 = {1,2,3},{3,4,5},{4,5,6}
+print(set1.union(set2,set3))                    # {1,2,3,4,5,6}
+print(set1)                                     # didn't change set1
+
+# set intersection
+# get the same value of given sets
+
+print(set1.intersection(set2))                  # {3}
+
+# set difference
+# return values that they're in set1 but not in the set2
+
+print(set1.difference(set2))                    # {1,2}
+
+# set symmetric_diffrence
+# return values that they're in set1 but not in the set2
+# and the opposite
+
+print(set1.symmetric_difference(set2))          # {1,2,4,5}
